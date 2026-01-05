3800852c351ca09ed5da97217bb629655f6197df
youxuehu <youxuehu@hotmail.com>
2026-01-05 22:55:56 +0800

    fix

--- app/api/v1/public/common/auth/[...path]/route.ts
+++ app/api/v1/public/common/auth/[...path]/route.ts
@@ -3,7 +3,7 @@ import { NextRequest, NextResponse } from "next/server";
 import { getServerSideConfig } from "@/app/config/server";
 
 const config = getServerSideConfig();
-const YEYING_BACKEND_URL = config.backend_url;
+const YEYING_BACKEND_URL = config.web_dav_backend_url;
 
 if (!YEYING_BACKEND_URL) {
   throw new Error("YEYING_BACKEND_URL is not set in environment variables");
